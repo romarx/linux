d52d6196fb23ebd987e17e99cebe914b87f576ee
romarx <roman.marquart@gmail.com>
2021-04-19 10:34:28 +0200

    gcon_cursor

--- drivers/video/console/gcon.c
+++ drivers/video/console/gcon.c
@@ -17,23 +17,22 @@
 #include <linux/slab.h>
 //#include <linux/dma-mapping.h>
 
-#define BLANK                   0x0020
-#define AH_BASE                 0x19000000
+#define BLANK 0x0020
+#define AH_BASE 0x19000000
 /*
   Offsets relative to AH_BASE
 */
-#define AH_PNTRQ_ADDR           0x0
-#define AH_HVTOT_REG_ADDR       0x8
-#define AH_HVACT_REG_ADDR       0x10
-#define AH_HVFRONT_REG_ADDR     0x18
-#define AH_HVSYNC_REG_ADDR      0x20
-#define AH_PWR_REG_ADDR         0x28
-#define AH_CURR_PNTR_ADDR       0x30
-#define AH_TEXT_PARAM_ADDR      0x38
-#define AH_CURSOR_PARAM_ADDR    0x40
-#define AH_FG_COLOR_START_ADDR  0x400
-#define AH_BG_COLOR_START_ADDR  0x800
-
+#define AH_PNTRQ_ADDR 0x0
+#define AH_HVTOT_REG_ADDR 0x8
+#define AH_HVACT_REG_ADDR 0x10
+#define AH_HVFRONT_REG_ADDR 0x18
+#define AH_HVSYNC_REG_ADDR 0x20
+#define AH_PWR_REG_ADDR 0x28
+#define AH_CURR_PNTR_ADDR 0x30
+#define AH_TEXT_PARAM_ADDR 0x38
+#define AH_CURSOR_PARAM_ADDR 0x40
+#define AH_FG_COLOR_START_ADDR 0x400
+#define AH_BG_COLOR_START_ADDR 0x800
 
 /*
   hardcode video format like a bad boy (SVGA60)
@@ -54,12 +53,12 @@
 #define GCON_TEXT_COLS 100
 
 static bool gcon_init_done = 0;
-static void * mapped_base = NULL;
+static void *mapped_base = NULL;
 
 static int font_factor;
 
-static unsigned short* blank_buf = NULL;
-static unsigned short* text_buf = NULL;
+static unsigned short *blank_buf = NULL;
+static unsigned short *text_buf = NULL;
 
 //would like to use this but doesn't work atm.
 //static dma_addr_t blank_buf_phys = 0, text_buf_phys = 0;
@@ -71,15 +70,18 @@ static unsigned short* text_buf = NULL;
 static void write_ah(int offset, u32 data);
 static u32 read_ah(int offset);
 static u32 read_current_p_ah(void);
-static void write_text_p_ah(u16*);
+static void write_text_p_ah(u16 *);
 static u32 gen_textparam_reg(u16 cols, u16 rows);
 static u32 gen_cursorparam_reg(u16 col, u16 row, u8 start, u8 end, u8 font_fac,
-                               u8 enable, u8 blink_t);
+			       u8 enable, u8 blink_t);
 
-
-static void dummycon_putc(struct vc_data *vc, int c, int ypos, int xpos) { }
+static void dummycon_putc(struct vc_data *vc, int c, int ypos, int xpos)
+{
+}
 static void dummycon_putcs(struct vc_data *vc, const unsigned short *s,
-			   int count, int ypos, int xpos) { }
+			   int count, int ypos, int xpos)
+{
+}
 static int dummycon_blank(struct vc_data *vc, int blank, int mode_switch)
 {
 	return 0;
@@ -90,42 +92,42 @@ static const char *gcon_startup(void)
 {
 	pr_info("Entered gcon_startup\n");
 	u8 max_fontfac_w, max_fontfac_h, max_fontfac;
-  	u8 fontfac_param;
-	if (gcon_init_done){
-    	return "AXI_HDMI Text Mode Console";
+	u8 fontfac_param;
+	if (gcon_init_done) {
+		return "AXI_HDMI Text Mode Console";
 	}
-	if (!request_mem_region(AH_BASE, 4096, "G Console AXI2HDMI driver")){
-    	printk(KERN_ALERT "Failed to reserve A2H IO Address Space!\n");
-    	return "AXI_HDMI Text Mode Console no memory reserved";
-  	}
+	if (!request_mem_region(AH_BASE, 4096, "G Console AXI2HDMI driver")) {
+		printk(KERN_ALERT "Failed to reserve A2H IO Address Space!\n");
+		return "AXI_HDMI Text Mode Console no memory reserved";
+	}
 	pr_info("requesting memory region succeeded!\n");
-	
+
 	if (!(mapped_base = ioremap((resource_size_t)AH_BASE, 4096))) {
-    	printk(KERN_ALERT "IOremap failed\n");
+		printk(KERN_ALERT "IOremap failed\n");
 		mapped_base = NULL;
-    	return "AXI_HDMI Text Mode Console no ioremap";
-  	}
+		return "AXI_HDMI Text Mode Console no ioremap";
+	}
 	pr_info("ioremap worked, mapped base: %pK\n", mapped_base);
 
-	
-	max_fontfac_w = GCON_VIDEO_COLS/(GCON_TEXT_COLS*GCON_FONTW);
-  	max_fontfac_h = GCON_VIDEO_LINES/(GCON_TEXT_ROWS*2*GCON_FONTW);
-  	max_fontfac = (max_fontfac_w < max_fontfac_h) ? max_fontfac_w : max_fontfac_h;
+	max_fontfac_w = GCON_VIDEO_COLS / (GCON_TEXT_COLS * GCON_FONTW);
+	max_fontfac_h = GCON_VIDEO_LINES / (GCON_TEXT_ROWS * 2 * GCON_FONTW);
+	max_fontfac =
+		(max_fontfac_w < max_fontfac_h) ? max_fontfac_w : max_fontfac_h;
 	fontfac_param = 0;
-  	switch(max_fontfac) {
-  	case 2:
-    	fontfac_param = 1;
-    	break;
-  	case 3:
-    	fontfac_param = 1;
-    	break;
-  	case 4:
-    	fontfac_param = 2;
-    	break;
-  	default:
-    	break;
-  	}
-	
+	switch (max_fontfac) {
+	case 2:
+		fontfac_param = 1;
+		break;
+	case 3:
+		fontfac_param = 1;
+		break;
+	case 4:
+		fontfac_param = 2;
+		break;
+	default:
+		break;
+	}
+
 	pr_info("fontfac param calculated\n");
 	/* this breaks everything
 
@@ -147,33 +149,37 @@ static const char *gcon_startup(void)
 	*/
 
 	//Using kmalloc (I don't know how safe this is yet)
-	if(!(blank_buf = kzalloc(GCON_TEXT_COLS*GCON_TEXT_ROWS*sizeof(u16), GFP_KERNEL))){
+	if (!(blank_buf = kzalloc(GCON_TEXT_COLS * GCON_TEXT_ROWS * sizeof(u16),
+				  GFP_KERNEL))) {
 		pr_info("Failed to allocate blank buffer memory with kzalloc.\n");
 		return "AXI_HDMI Text Mode Console no blank buf";
 	}
 	pr_info("kzalloc worked for blank buf\n");
 
-	if(!(text_buf = kzalloc(GCON_TEXT_COLS*GCON_TEXT_ROWS*sizeof(u16), GFP_KERNEL))){
+	if (!(text_buf = kzalloc(GCON_TEXT_COLS * GCON_TEXT_ROWS * sizeof(u16),
+				 GFP_KERNEL))) {
 		pr_info("Failed to allocate text buffer memory with kzalloc.\n");
 		return "AXI_HDMI Text Mode Console no text buf";
 	}
 	pr_info("kzalloc worked for text buf\n");
 
-
 	// on startup, power off AXI_HDMI
-  	write_ah(AH_PWR_REG_ADDR, 0);
-  	write_ah(AH_TEXT_PARAM_ADDR, gen_textparam_reg(GCON_TEXT_COLS, GCON_TEXT_ROWS));
+	write_ah(AH_PWR_REG_ADDR, 0);
+	write_ah(AH_TEXT_PARAM_ADDR,
+		 gen_textparam_reg(GCON_TEXT_COLS, GCON_TEXT_ROWS));
 	// cursor off by default
-  	write_ah(AH_CURSOR_PARAM_ADDR, gen_cursorparam_reg(0,0,0,2*GCON_FONTW-1, fontfac_param, 0, GCON_BLINK_T));
+	write_ah(AH_CURSOR_PARAM_ADDR,
+		 gen_cursorparam_reg(0, 0, 0, 2 * GCON_FONTW - 1, fontfac_param,
+				     0, GCON_BLINK_T));
 	//set timings
-	write_ah(AH_HVACT_REG_ADDR, (GCON_VIDEO_COLS<<16) + GCON_VIDEO_LINES);
-  	write_ah(AH_HVTOT_REG_ADDR, (GCON_HTOT<<16) + GCON_VTOT);
-  	write_ah(AH_HVFRONT_REG_ADDR, (GCON_HFRONT<<16) + GCON_VFRONT);
-  	write_ah(AH_HVSYNC_REG_ADDR, (GCON_HSYNC<<16) + GCON_VSYNC);
-  	
+	write_ah(AH_HVACT_REG_ADDR, (GCON_VIDEO_COLS << 16) + GCON_VIDEO_LINES);
+	write_ah(AH_HVTOT_REG_ADDR, (GCON_HTOT << 16) + GCON_VTOT);
+	write_ah(AH_HVFRONT_REG_ADDR, (GCON_HFRONT << 16) + GCON_VFRONT);
+	write_ah(AH_HVSYNC_REG_ADDR, (GCON_HSYNC << 16) + GCON_VSYNC);
+
 	font_factor = fontfac_param;
 	gcon_init_done = 1;
-    
+
 	pr_info("gcon startup done\n");
 	return "AXI_HDMI Text Mode Console";
 }
@@ -181,40 +187,65 @@ static const char *gcon_startup(void)
 static void gcon_init(struct vc_data *vc, int init)
 {
 	pr_info("Entered gcon_init\n");
-    vc->vc_can_do_color = 1;
-    if (init) {
+	vc->vc_can_do_color = 1;
+	if (init) {
 		vc->vc_cols = GCON_TEXT_COLS;
 		vc->vc_rows = GCON_TEXT_ROWS;
-    } else {
+	} else {
 		vc_resize(vc, GCON_TEXT_COLS, GCON_TEXT_ROWS);
 	}
 
 	vc->vc_scan_lines = GCON_VIDEO_LINES;
-  	vc->vc_font.height = 2*GCON_FONTW;
-  	vc->vc_complement_mask = 0x7700;
-  	vc->vc_hi_font_mask = 0;
+	vc->vc_font.height = 2 * GCON_FONTW;
+	vc->vc_complement_mask = 0x7700;
+	vc->vc_hi_font_mask = 0;
 	pr_info("Finish gcon_init\n");
-
 }
 
-static void gcon_deinit(struct vc_data *vc) {
+static void gcon_deinit(struct vc_data *vc)
+{
 	pr_info("Entered gcon_deinit");
 	iounmap(mapped_base);
 	release_mem_region(AH_BASE, 4096);
 	mapped_base = NULL;
-	if(blank_buf){
-		kfree((void*) blank_buf);
+	if (blank_buf) {
+		kfree((void *)blank_buf);
 	}
-	if(text_buf){
-		kfree((void*) text_buf);
+	if (text_buf) {
+		kfree((void *)text_buf);
 	}
 
 	//TODO: deallocate dma memory
-
 }
 static void dummycon_clear(struct vc_data *vc, int sy, int sx, int height,
-			   int width) { }
-static void dummycon_cursor(struct vc_data *vc, int mode) { }
+			   int width)
+{
+}
+
+static void gcon_cursor(struct vc_data *vc, int mode)
+{
+	pr_info("Entered gcon_cursor");
+	u32 cur = read_ah(AH_CURSOR_PARAM_ADDR);
+	switch (mode) {
+	case CM_ERASE:
+		cur &= 0xffffdfff;
+		break;
+	case CM_MOVE:
+	case CM_DRAW:
+		/* for now only check for CUR_NONE */
+		if ((c->vc_cursor_type & 0x0f) == CUR_NONE)
+			cur &= 0xffffdfff;
+		else {
+			int x, y;
+			gcon_getxy(c, c->vc_pos, &x, &y);
+			cur |= 0x00002000;
+			cur &= 0x0000ffff;
+			cur |= x << 24;
+			cur |= y << 16;
+		}
+	}
+	write_ah(AH_CURSOR_PARAM_ADDR, cur);
+}
 
 static bool dummycon_scroll(struct vc_data *vc, unsigned int top,
 			    unsigned int bottom, enum con_scroll dir,
@@ -225,6 +256,7 @@ static bool dummycon_scroll(struct vc_data *vc, unsigned int top,
 
 static int gcon_switch(struct vc_data *vc)
 {
+	pr_info("Entered gcon switch");
 	return 1;
 }
 
@@ -234,8 +266,8 @@ static int dummycon_font_set(struct vc_data *vc, struct console_font *font,
 	return 0;
 }
 
-static int dummycon_font_default(struct vc_data *vc,
-				 struct console_font *font, char *name)
+static int dummycon_font_default(struct vc_data *vc, struct console_font *font,
+				 char *name)
 {
 	return 0;
 }
@@ -249,15 +281,18 @@ static int dummycon_font_copy(struct vc_data *vc, int con)
    Internal Functions
    -------------------------------- */
 
-static void write_ah(int offset, u32 data) {
-  if (mapped_base)
-    writel(data, (volatile void *) mapped_base+offset);
+static void write_ah(int offset, u32 data)
+{
+	if (mapped_base)
+		writel(data, (volatile void *)mapped_base + offset);
 }
 
-static u32 read_ah(int offset) {
-  if (mapped_base)
-    return readl((const volatile void *) mapped_base+offset);
-  else return 0;
+static u32 read_ah(int offset)
+{
+	if (mapped_base)
+		return readl((const volatile void *)mapped_base + offset);
+	else
+		return 0;
 }
 
 /* 
@@ -265,48 +300,52 @@ keeps power status reg as-is
 
 original has a dma_addr_t instead of u16*
 */
-static void write_text_p_ah(u16 *p) {
-  u32 pwr = read_ah(AH_PWR_REG_ADDR);
-  pwr |= (1<<16);
-  write_ah(AH_PWR_REG_ADDR, pwr);
-  write_ah(AH_PNTRQ_ADDR, (u32) p);
+static void write_text_p_ah(u16 *p)
+{
+	u32 pwr = read_ah(AH_PWR_REG_ADDR);
+	pwr |= (1 << 16);
+	write_ah(AH_PWR_REG_ADDR, pwr);
+	write_ah(AH_PNTRQ_ADDR, (u32)p);
 }
 
- static u32 read_current_p_ah(void) {
-   return read_ah(AH_CURR_PNTR_ADDR);
- }
+static u32 read_current_p_ah(void)
+{
+	return read_ah(AH_CURR_PNTR_ADDR);
+}
 
-static u32 gen_textparam_reg(u16 cols, u16 rows) {
-  return ((cols<<16)+rows);
+static u32 gen_textparam_reg(u16 cols, u16 rows)
+{
+	return ((cols << 16) + rows);
 }
 
 static u32 gen_cursorparam_reg(u16 col, u16 row, u8 start, u8 end, u8 font_fac,
-                               u8 enable, u8 blink_t) {
-  u32 curs_reg = 0;
-  curs_reg |= (col << 24);
-  curs_reg |= (row << 16);
-  curs_reg |= (font_fac << 14);
-  curs_reg |= (enable << 13);
-  curs_reg |= (start << 8);
-  curs_reg |= (blink_t << 5);
-  curs_reg |= (end << 0);
-  return curs_reg;
+			       u8 enable, u8 blink_t)
+{
+	u32 curs_reg = 0;
+	curs_reg |= (col << 24);
+	curs_reg |= (row << 16);
+	curs_reg |= (font_fac << 14);
+	curs_reg |= (enable << 13);
+	curs_reg |= (start << 8);
+	curs_reg |= (blink_t << 5);
+	curs_reg |= (end << 0);
+	return curs_reg;
 }
 
 const struct consw gcon = {
-	.owner =		THIS_MODULE,
-	.con_startup =	gcon_startup,
-	.con_init =		gcon_init,
-	.con_deinit =	gcon_deinit,
-	.con_clear =	dummycon_clear,
-	.con_putc =		dummycon_putc,
-	.con_putcs =	dummycon_putcs,
-	.con_cursor =	dummycon_cursor,
-	.con_scroll =	dummycon_scroll,
-	.con_switch =	gcon_switch,
-	.con_blank =	dummycon_blank,
-	.con_font_set =	dummycon_font_set,
-	.con_font_default =	dummycon_font_default,
-	.con_font_copy =	dummycon_font_copy,
+	.owner = THIS_MODULE,
+	.con_startup = gcon_startup,
+	.con_init = gcon_init,
+	.con_deinit = gcon_deinit,
+	.con_clear = dummycon_clear,
+	.con_putc = dummycon_putc,
+	.con_putcs = dummycon_putcs,
+	.con_cursor = gcon_cursor,
+	.con_scroll = dummycon_scroll,
+	.con_switch = gcon_switch,
+	.con_blank = dummycon_blank,
+	.con_font_set = dummycon_font_set,
+	.con_font_default = dummycon_font_default,
+	.con_font_copy = dummycon_font_copy,
 };
 EXPORT_SYMBOL_GPL(gcon);

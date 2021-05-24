39b7ae02204c7f2e07c9153d711611814162e8ab
romarx <roman.marquart@gmail.com>
2021-05-24 18:29:46 +0200

    reset formatting

--- drivers/video/fbdev/paperfb.c
+++ drivers/video/fbdev/paperfb.c
@@ -3,15 +3,14 @@
 #include <linux/errno.h>
 #include <linux/fb.h>
 #include <linux/init.h>
-#include <linux/io.h> 
+#include <linux/io.h>
 #include <linux/kernel.h>
-#include <linux/mm.h> 
+#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/string.h>
 #include <linux/slab.h>
-
 
 #define AH_PNTRQ_ADDR 0x0
 #define AH_HVTOT_REG_ADDR 0x8
@@ -25,7 +24,7 @@
 #define AH_FG_COLOR_START_ADDR 0x400
 #define AH_BG_COLOR_START_ADDR 0x800
 
-#define PALETTE_SIZE	256
+#define PALETTE_SIZE 256
 
 #define PAPERFB_NAME "PAPER AXI/RGB FB"
 
@@ -35,22 +34,31 @@ static char *mode_option;
 static const struct fb_videomode default_mode = {
 	/* 800x600 @ 60 Hz, 37.8 kHz hsync */
 	/* NULL, <Freq>, <hact>, <vact>, <?>, <hback>, <hfront>, <vback>, <vfront>, <hsync>, <vsync>, <hsyncpol | vsyncpol>, <vmode>, <isvesa>*/
-	NULL, 60, 800, 600, 25000, 88, 40, 23, 1, 128, 4,
-		FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
-		FB_VMODE_NONINTERLACED 
+	NULL,
+	60,
+	800,
+	600,
+	25000,
+	88,
+	40,
+	23,
+	1,
+	128,
+	4,
+	FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+	FB_VMODE_NONINTERLACED
 };
 
 struct paperfb_dev {
 	struct fb_info info;
-	void __iomem *regs; 
+	void __iomem *regs;
 	/* flag indicating whether the regs are little endian accessed */
 	int little_endian;
 	/* Physical and virtual addresses of framebuffer */
 	dma_addr_t fb_phys;
 	void __iomem *fb_virt;
-	u32 pseudo_palette[PALETTE_SIZE]; 
+	u32 pseudo_palette[PALETTE_SIZE];
 };
-
 
 #ifndef MODULE
 static int __init paperfb_setup(char *options)
@@ -93,17 +101,20 @@ static int paperfb_setupfb(struct paperfb_dev *fbdev)
 	/* Disable display */
 	paperfb_writereg(fbdev, AH_PWR_REG_ADDR, 0);
 
-	
 	/* length and width of frame*/
-	paperfb_writereg(fbdev, AH_HVACT_REG_ADDR, (var->xres << 16) + var->yres);
+	paperfb_writereg(fbdev, AH_HVACT_REG_ADDR,
+			 (var->xres << 16) + var->yres);
 
 	/* Total length and width of frame */
-	hlen = var->left_margin + var->right_margin + var->hsync_len + var->xres;
-	vlen = var->upper_margin + var->lower_margin + var->vsync_len + var->yres;
+	hlen = var->left_margin + var->right_margin + var->hsync_len +
+	       var->xres;
+	vlen = var->upper_margin + var->lower_margin + var->vsync_len +
+	       var->yres;
 	paperfb_writereg(fbdev, AH_HVTOT_REG_ADDR, (hlen << 16) + vlen);
 
 	//TODO: Is this right???
-	paperfb_writereg(fbdev, AH_HVFRONT_REG_ADDR, (var->right_margin << 16) + var->lower_margin);
+	paperfb_writereg(fbdev, AH_HVFRONT_REG_ADDR,
+			 (var->right_margin << 16) + var->lower_margin);
 
 	/* Horizontal and verical sync */
 	hvsync = (var->hsync_len << 16) + var->vsync_len;
@@ -116,7 +127,7 @@ static int paperfb_setupfb(struct paperfb_dev *fbdev)
 	paperfb_writereg(fbdev, AH_HVSYNC_REG_ADDR, hvsync);
 
 	/* Put framebuffer address into queue */
-	
+
 	paperfb_writereg(fbdev, AH_PNTRQ_ADDR, fbdev->fb_phys);
 
 	//check addresses
@@ -128,19 +139,18 @@ static int paperfb_setupfb(struct paperfb_dev *fbdev)
 	pr_info("PWRREG:\t0x%x\n", paperfb_readreg(fbdev, AH_PWR_REG_ADDR));
 	pr_info("CURPTR:\t0x%x\n", paperfb_readreg(fbdev, AH_CURR_PNTR_ADDR));
 	pr_info("TXTPRM:\t0x%x\n", paperfb_readreg(fbdev, AH_TEXT_PARAM_ADDR));
-	pr_info("CURPRM:\t0x%x\n", paperfb_readreg(fbdev, AH_CURSOR_PARAM_ADDR));
+	pr_info("CURPRM:\t0x%x\n",
+		paperfb_readreg(fbdev, AH_CURSOR_PARAM_ADDR));
 
 	/* Enable display */
 	paperfb_writereg(fbdev, AH_PWR_REG_ADDR, 1);
 	return 0;
-
 }
 
 static int paperfb_setcolreg(unsigned regno, unsigned red, unsigned green,
-			  unsigned blue, unsigned transp,
-			  struct fb_info *info)
+			     unsigned blue, unsigned transp,
+			     struct fb_info *info)
 {
-
 	if (regno >= info->cmap.len) {
 		dev_err(info->device, "regno >= cmap.len\n");
 		return 1;
@@ -151,9 +161,9 @@ static int paperfb_setcolreg(unsigned regno, unsigned red, unsigned green,
 	blue >>= (16 - info->var.blue.length);
 
 	((u32 *)(info->pseudo_palette))[regno] =
-			(red << info->var.red.offset) |
-			(green << info->var.green.offset) |
-			(blue << info->var.blue.offset);
+		(red << info->var.red.offset) |
+		(green << info->var.green.offset) |
+		(blue << info->var.blue.offset);
 	return 0;
 }
 
@@ -164,7 +174,7 @@ static int paperfb_init_fix(struct paperfb_dev *fbdev)
 
 	strcpy(fix->id, PAPERFB_NAME);
 
-	fix->line_length = var->xres * var->bits_per_pixel/8;
+	fix->line_length = var->xres * var->bits_per_pixel / 8;
 	fix->smem_len = fix->line_length * var->yres;
 	fix->type = FB_TYPE_PACKED_PIXELS;
 
@@ -183,10 +193,10 @@ static int paperfb_init_var(struct paperfb_dev *fbdev)
 	var->xres_virtual = var->xres;
 	var->yres_virtual = var->yres;
 
-	if(var->bits_per_pixel != 24){
+	if (var->bits_per_pixel != 24) {
 		dev_err(dev, "Colour depth not supported!\n");
 		return -EINVAL;
-	}else{
+	} else {
 		var->transp.offset = 0;
 		var->transp.length = 0;
 		var->red.offset = 16;
@@ -199,15 +209,13 @@ static int paperfb_init_var(struct paperfb_dev *fbdev)
 	return 0;
 }
 
-
 static struct fb_ops paperfb_ops = {
-	.owner		= THIS_MODULE,
-	.fb_setcolreg	= paperfb_setcolreg, 
-	.fb_fillrect	= cfb_fillrect, 
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
+	.owner = THIS_MODULE,
+	.fb_setcolreg = paperfb_setcolreg,
+	.fb_fillrect = cfb_fillrect,
+	.fb_copyarea = cfb_copyarea,
+	.fb_imageblit = cfb_imageblit,
 };
-
 
 static int paperfb_probe(struct platform_device *pdev)
 {
@@ -221,30 +229,30 @@ static int paperfb_probe(struct platform_device *pdev)
 	fbdev = devm_kzalloc(&pdev->dev, sizeof(*fbdev), GFP_KERNEL);
 	if (!fbdev)
 		return -ENOMEM;
-	
+
 	pr_info("fbdev alloc success");
 
 	platform_set_drvdata(pdev, fbdev);
 
-	fbdev->info.fbops = &paperfb_ops; 
+	fbdev->info.fbops = &paperfb_ops;
 	fbdev->info.device = &pdev->dev;
 	fbdev->info.par = fbdev;
 
-    /* Video mode setup */
-	if (!fb_find_mode(&fbdev->info.var, &fbdev->info, mode_option,
-			  NULL, 0, &default_mode, 24)) {
+	/* Video mode setup */
+	if (!fb_find_mode(&fbdev->info.var, &fbdev->info, mode_option, NULL, 0,
+			  &default_mode, 24)) {
 		dev_err(&pdev->dev, "No valid video modes found\n");
 		return -EINVAL;
 	}
 
 	pr_info("set video mode success");
-	
-	paperfb_init_var(fbdev); 
+
+	paperfb_init_var(fbdev);
 	paperfb_init_fix(fbdev);
 
-	pr_info("init var and fix success"); 
+	pr_info("init var and fix success");
 
-    /* Request I/O resource */
+	/* Request I/O resource */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res) {
 		dev_err(&pdev->dev, "I/O resource request failed\n");
@@ -254,16 +262,16 @@ static int paperfb_probe(struct platform_device *pdev)
 	pr_info("resource request success");
 
 	fbdev->regs = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(fbdev->regs)){
+	if (IS_ERR(fbdev->regs)) {
 		return PTR_ERR(fbdev->regs);
 	}
 	pr_info("remap of IO success");
 
-    /* Allocate framebuffer memory */
-    // without dma engine, maybe try to activate it once and try on gcon...
+	/* Allocate framebuffer memory */
+	// without dma engine, maybe try to activate it once and try on gcon...
 	fbsize = fbdev->info.fix.smem_len;
 	fbdev->fb_virt = kzalloc(fbsize, GFP_KERNEL);
-	
+
 	if (!fbdev->fb_virt) {
 		dev_err(&pdev->dev, "Frame buffer memory allocation failed\n");
 		return -ENOMEM;
@@ -274,10 +282,10 @@ static int paperfb_probe(struct platform_device *pdev)
 	fbdev->info.screen_base = fbdev->fb_virt;
 	fbdev->info.pseudo_palette = fbdev->pseudo_palette;
 
-    /* Setup and enable the framebuffer */
+	/* Setup and enable the framebuffer */
 	paperfb_setupfb(fbdev);
 
-    //check what kind of Eindianness it has
+	//check what kind of Eindianness it has
 	//fbdev->info.flags |= FBINFO_FOREIGN_ENDIAN;
 
 	/* Allocate color map */
@@ -287,8 +295,7 @@ static int paperfb_probe(struct platform_device *pdev)
 		kfree(fbdev->fb_virt);
 	}
 
-
-    /* Register framebuffer */
+	/* Register framebuffer */
 	ret = register_framebuffer(&fbdev->info);
 	if (ret) {
 		dev_err(&pdev->dev, "Framebuffer registration failed\n");
@@ -315,24 +322,22 @@ static int paperfb_remove(struct platform_device *pdev)
 	return 0;
 }
 
-
-
 static struct of_device_id paperfb_match[] = {
-	{ .compatible = "paper,paperfb", },
+	{
+		.compatible = "paper,paperfb",
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, paperfb_match);
 
-
-static struct platform_driver paperfb_driver = {
-	.probe  = paperfb_probe,
-	.remove	= paperfb_remove,
+static struct platform_driver paperfb_driver = { 
+        .probe = paperfb_probe,
+	.remove = paperfb_remove,
 	.driver = {
-		.name = "paper_fb",
-		.of_match_table = paperfb_match,		
-	}
+	        .name = "paper_fb",
+	        .of_match_table = paperfb_match,
+	} 
 };
-
 
 /*
  * Init and exit routines
